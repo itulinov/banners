925ada717fb60c9e42403d0db30c98f49d756aaf
iTulinov <energiay@gmail.com>
2025-04-07 18:00:05 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const packageJson = require('./package.json')
+
+module.exports = {
+    url: 'https://protutors.ru/view_doc.html?mode=banners_dev',
+    publicPath: packageJson.homepage,
+    cookieString: 'SessionID=5545373038429523708; ' +
+                  'x-auth-id=cc48385f6fdc4d8898515c723ae3c00c'
+}
---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Classes from "./App.module.css"
+import "./index.css"
+
+
+function App({settings}) {
+    console.log(settings)
+    return (
+        <div className={Classes.App}>
+            BANNERS
+        </div>
+    )
+}
+
+export default App
